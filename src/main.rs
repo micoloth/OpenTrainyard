155f2a0e937e7ad9f5ada4c39cd1bca857bcc85a
michele-tasca <michele.tasca@adevinta.com>
2022-12-25 18:46:59 +0100

    WORKING player in main

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,57 @@ use crate::player::ActionsPlugin;
 use crate::audio::InternalAudioPlugin;
 use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
-use crate::player::PlayerPlugin;
+
+use crate::loading::TextureAssets;
+pub struct PlayerPlugin;
+
+#[derive(Component)]
+pub struct Player;
+
+
+use crate::player::Actions;
+
+fn move_player(
+    time: Res<Time>,
+    actions: Res<Actions>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+) {
+    if actions.player_movement.is_none() {
+        return;
+    }
+    let speed = 150.;
+    let movement = Vec3::new(
+        actions.player_movement.unwrap().x * speed * time.delta_seconds(),
+        actions.player_movement.unwrap().y * speed * time.delta_seconds(),
+        0.,
+    );
+    for mut player_transform in &mut player_query {
+        player_transform.translation += movement;
+    }
+}
+
+
+/// This plugin handles player related stuff like movement
+/// Player logic is only active during the State `GameState::Playing`
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_player))
+            .add_system_set(SystemSet::on_update(GameState::Playing).with_system(move_player));
+    }
+}
+
+fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: textures.texture_bevy.clone(),
+            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            ..Default::default()
+        })
+        .insert(Player);
+}
+
+
+
 
 // use bevy::app::App;
 #[cfg(debug_assertions)]
